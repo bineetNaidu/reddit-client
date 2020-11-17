f4d53e07e8c01cf09cc72962d9ad4540d9206416
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-17 20:05:46 +0530

    -- remove unused files -- improve navbar -- add bootstrap

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,29 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Nav from '../components/Nav.svelte';
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<Nav />
+
+<svelte:head>
+  <!-- CSS only -->
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ"
+    crossorigin="anonymous" />
+</svelte:head>
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
