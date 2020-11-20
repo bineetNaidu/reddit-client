a04be214065ac378f88225e564c22e84f69fa445
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-20 12:02:28 +0530

    moved redditpost ot its component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import Loaders from './Loaders.svelte';
   import Navbar from './Navbar.svelte';
+  import RedditPost from './RedditPost.svelte';
   $: posts = [];
   let loading = true;
   onMount(async () => {
@@ -30,29 +31,7 @@
 {:else}
   <div class="row row-cols-1 row-cols-md-4 g-4">
     {#each posts as { data } (data.id)}
-      <div class="col">
-        <div class="card" style="width: 18rem;">
-          {#if data.is_video}
-            <video controls width="auto" autoplay muted>
-              <track kind="captions" />
-              <source
-                src={data.media.reddit_video.scrubber_media_url || data.media.reddit_video.fallback_url}
-                type="video/mp4" />
-
-              Sorry, your browser doesn't support embedded videos.
-            </video>
-          {:else}
-            <img
-              src={data.thumbnail || data.url || data.preview.images[0].source.url}
-              class="card-img-top"
-              alt={data.thumbnail || data.url} />
-          {/if}
-          <div class="card-body">
-            <p class="card-text">{data.title}</p>
-            <a href={data.permalink} class="btn btn-primary">View</a>
-          </div>
-        </div>
-      </div>
+      <RedditPost {data} />
     {/each}
   </div>
 {/if}
